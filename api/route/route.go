774214a0b03535432f413c20b09d230fffeeeaec
Ayabonga <ayabongabooi2@gmail.com>
2024-03-05 18:29:55 +0200

    feat: add health checker router

--- api/route/route.go
+++ api/route/route.go
@@ -9,5 +9,16 @@ import (
 )
 
 func Setup(env *bootstrap.Env, timeout time.Duration, db mongo.Database, gin *gin.Engine) {
+	publicRouter := gin.Group("")
+	// All Public APIs
+	healthCheckRouter(publicRouter)
 
+}
+
+func healthCheckRouter(group *gin.RouterGroup) {
+	group.GET("/healthcheck", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "All systems work fine.",
+		})
+	})
 }
