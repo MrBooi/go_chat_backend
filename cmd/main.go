bc34baa2bf85ed764a2b517754badfa27dc01678
Ayabonga <ayabongabooi2@gmail.com>
2024-03-05 12:40:19 +0200

    feat: use env into the main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/MrBooi/go_chat_backend/bootstrap"
+)
+
+func main() {
+	app := bootstrap.App()
+
+	env := app.Env
+
+	fmt.Print(env.AppEnv)
 }
