f4e0dcee4fbe2a32d3d7bd4117becdd2f09231c2
Ayabonga <ayabongabooi2@gmail.com>
2024-03-17 20:12:07 +0200

    feat: add new function for create and refresh token impl

--- usecase/register_usecase.go
+++ usecase/register_usecase.go
@@ -5,11 +5,22 @@ import (
 	"time"
 
 	"github.com/MrBooi/go_chat_backend/domain"
+	"github.com/MrBooi/go_chat_backend/internal/tokenutil"
 )
 
 type registerUsecase struct {
 	userRepository domain.UserRepository
 	contextTimeout time.Duration
+}
+
+// CreateAccessToken implements domain.RegisterUsecase.
+func (r *registerUsecase) CreateAccessToken(user *domain.User, secret string, expiry int) (accessToken string, err error) {
+	return tokenutil.CreateAccessToken(user, secret, expiry)
+}
+
+// CreateRefreshToken implements domain.RegisterUsecase.
+func (r *registerUsecase) CreateRefreshToken(user *domain.User, secret string, expiry int) (refreshToken string, err error) {
+	return tokenutil.CreateRefreshToken(user, secret, expiry)
 }
 
 func (r *registerUsecase) Create(c context.Context, user *domain.User) error {
