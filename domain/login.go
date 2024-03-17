390551816a110aed1b82f3495191d8454251861b
Ayabonga <ayabongabooi2@gmail.com>
2024-03-17 20:11:52 +0200

    feat: add new function for create and refresh token impl

--- domain/login.go
+++ domain/login.go
@@ -6,6 +6,13 @@ type LoginRequest struct {
 	Uuid string `json:"uuid" binding:"required"`
 }
 
+type LoginResponse struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
 type LoginUsecase interface {
 	GetUserByUuid(c context.Context, uuid string) (User, error)
+	CreateAccessToken(user *User, secret string, expiry int) (accessToken string, err error)
+	CreateRefreshToken(user *User, secret string, expiry int) (refreshToken string, err error)
 }
