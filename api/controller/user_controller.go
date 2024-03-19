dbc195b9b896cf2dc8cddf97ff7c655bd59d4644
Ayabonga <ayabongabooi2@gmail.com>
2024-03-19 21:21:01 +0200

    wip: update user controller by adding getProfile and update profile

--- api/controller/user_controller.go
+++ api/controller/user_controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/MrBooi/go_chat_backend/bootstrap"
@@ -15,6 +14,28 @@ type UserController struct {
 	Env         *bootstrap.Env
 }
 
+/**
+ * Gets the current user's profile
+ */
+func (uc *UserController) Profile(c *gin.Context) {
+	userID := c.GetString("x-user-id")
+	_, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	user, err := uc.UserUsecase.GetProfile(c, userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+/**
+ * Update current user's account
+ */
 func (uc *UserController) UpdateUser(c *gin.Context) {
 	var body domain.UpdateUserRequest
 
@@ -25,20 +46,18 @@ func (uc *UserController) UpdateUser(c *gin.Context) {
 	}
 
 	userID := c.GetString("x-user-id")
-	fmt.Println(userID)
 	_, err = primitive.ObjectIDFromHex(userID)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: err.Error()})
 		return
 	}
 
-	_, err = uc.UserUsecase.UpdateUser(c, userID, body)
+	user, err := uc.UserUsecase.UpdateUser(c, userID, body)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, "Updated successfully")
-
+	c.JSON(http.StatusOK, user)
 }
